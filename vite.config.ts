9df53f38f8f1c1549f48eedb3a29a5c3fbb89638
MontgomBig2 <zachmonty50@icloud.com>
2026-02-11 15:32:52 -0700

    Set base path for the project in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ADD THIS LINE BELOW
+      base: '/Mine-Finance-Website/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
